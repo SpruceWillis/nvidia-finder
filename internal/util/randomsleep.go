de9ada96b1a03b92eede98787cd68322e873e2a4
Kevin Huang <kevinht.huang@gmail.com>
2020-10-15 00:20:26 -0700

    add docs for utils

--- internal/util/randomsleep.go
+++ internal/util/randomsleep.go
@@ -1,12 +1,13 @@
 package util
 
 import (
-  "math/rand"
-  "time"
+	"math/rand"
+	"time"
 )
 
-func RandomSleep(lowerBound, upperBound int){
-  diff := upperBound - lowerBound
-  sleepTime := rand.Intn(diff + 1) + lowerBound
-  time.Sleep(time.Duration(sleepTime) * time.Second)
+// RandomSleep for a randomly determined number of seconds between the lower and upper bounds
+func RandomSleep(lowerBound, upperBound int) {
+	diff := upperBound - lowerBound
+	sleepTime := rand.Intn(diff+1) + lowerBound
+	time.Sleep(time.Duration(sleepTime) * time.Second)
 }
