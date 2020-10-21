6088b097a1ffd7d807e95960c561f171483e27da
Kevin Huang <kevinht.huang@gmail.com>
2020-10-21 00:58:07 -0700

    add testing script for email sending

---
+++ cmd/test_email.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+import (
+	"github.com/sprucewillis/nvidia-finder/internal/email"
+	"github.com/sprucewillis/nvidia-finder/internal/email/auth"
+)
+
+func main() {
+	creds := auth.GetSmtpCreds()
+	plainAuth := creds.GetPlainAuth()
+	from := creds.Username
+	emailDomain := "@gmail.com"
+	// TODO store the recipient list somewhere else
+	to := []string{(creds.Username + emailDomain)}
+	email.Send("test", creds.GetURL(), from, to, plainAuth)
+}
+*/
