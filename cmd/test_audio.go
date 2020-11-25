a4949542bbcceeeb269bf8c27483c3e39cc1eb5f
Kevin Huang <kevinht.huang@gmail.com>
2020-11-24 23:44:03 -0800

    add test for audio, commented out to appease the IDE gods

---
+++ cmd/test_audio.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+import (
+	"log"
+
+	"github.com/sprucewillis/nvidia-finder/internal/util/alerts"
+	"github.com/sprucewillis/nvidia-finder/internal/webscraper/inventory"
+)
+
+func main() {
+	c := make(chan inventory.Item)
+	go alerts.SetupAudioAlerts(c)
+	card := inventory.Item{URL: "https://youtu.be/eUuYgmWq9F4", Name: "Shibe Card",
+		Site: "Youtube", PriceLimit: 0.0, Sku: "Doge"}
+	log.Println("card", card)
+	c <- card
+	select {}
+}
+*/
