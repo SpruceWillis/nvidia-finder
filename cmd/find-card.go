70c5b817290369430718e5d3e4d47e68cc7f2b63
Kevin Huang <kevinht.huang@gmail.com>
2020-11-27 00:33:21 -0800

    refactor for easier organizing

--- cmd/find-card.go
+++ cmd/find-card.go
@@ -6,6 +6,7 @@ import (
 	"github.com/sprucewillis/nvidia-finder/internal/util/alerts"
 	scraper "github.com/sprucewillis/nvidia-finder/internal/webscraper"
 	"github.com/sprucewillis/nvidia-finder/internal/webscraper/inventory"
+	"github.com/sprucewillis/nvidia-finder/internal/webscraper/newegg"
 )
 
 func main() {
@@ -16,7 +17,10 @@ func main() {
 	go alerts.SetupAudioAlerts(audioAlertChannel)
 	alertChannel := alerts.SetUpAlertChannel([]chan inventory.Item{emailAlertChannel, audioAlertChannel})
 	go scraper.CheckBestBuy(client, false, alertChannel)
-	go scraper.CheckNewegg(client, alertChannel)
+	neweggConfig, err := newegg.ReadNeweggConfig()
+	if err == nil {
+		go newegg.CheckNewegg(client, neweggConfig, alertChannel)
+	}
 	select {
 	// keep program alive
 	}
--- internal/webscraper/newegg/go.mod
+++ internal/webscraper/newegg/go.mod
@@ -1,3 +1,9 @@
 module github.com/sprucewillis/nvidia-finder/internal/webscraper/newegg
 
 go 1.15
+
+require (
+	github.com/sprucewillis/nvidia-finder/internal/util v0.0.0-20201126032723-990c2a96a21c
+	github.com/sprucewillis/nvidia-finder/internal/webscraper/inventory v0.0.0-20201126032723-990c2a96a21c
+	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
+)
