bb5e839ff48d674a29a659d4565a8134c55b01c3
Kevin Huang <kevinht.huang@gmail.com>
2020-12-05 23:38:09 -0800

    add function for finding by attribute to make things easier

--- internal/util/htmlutils/child_finder.go
+++ internal/util/htmlutils/child_finder.go
@@ -1,6 +1,8 @@
 package htmlutils
 
-import "golang.org/x/net/html"
+import (
+	"golang.org/x/net/html"
+)
 
 // FindChild find a child node that meets a condition. Returns nil if not found
 func FindChild(node *html.Node, childConditions func(*html.Node) bool) *html.Node {
@@ -12,4 +14,17 @@ func FindChild(node *html.Node, childConditions func(*html.Node) bool) *html.Nod
 		child = child.NextSibling
 	}
 	return nil
+}
+
+// FindChildByAttribute find a child node with a certain attribute - use nil if unset
+func FindChildByAttribute(node *html.Node, targetAttr html.Attribute) *html.Node {
+	condition := func(n *html.Node) bool {
+		for _, attr := range n.Attr {
+			if attr.Namespace == targetAttr.Namespace && attr.Key == targetAttr.Key && attr.Val == targetAttr.Val {
+				return true
+			}
+		}
+		return false
+	}
+	return FindChild(node, condition)
 }
